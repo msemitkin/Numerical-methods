0f59c74dbcf8828fe84b75a9d2fc6cf3c7271486
Mykhailo Semitkin <michael.semitkin@gmail.com>
2021-04-02 00:43:01 +0300

    implement ranking

--- lab3/pagerank/main.py
+++ lab3/pagerank/main.py
@@ -3,8 +3,10 @@ import pagerank
 
 
 def run():
+    epsilon = 0.001
     edges = numpy.array([[1, 5], [2, 1], [2, 3], [3, 4], [3, 5], [4, 2], [5, 4], [5, 2]])
-    print(pagerank.get_markov_matrix(edges))
+    solution = pagerank.find_page_ranks_vector(edges, epsilon)
+    print(f"solution: {solution}")
 
 
 if __name__ == "__main__":
--- lab3/pagerank/pagerank.py
+++ lab3/pagerank/pagerank.py
@@ -2,6 +2,24 @@ import numpy
 import collections
 
 
+def find_page_ranks_vector(graph_edges, epsilon):
+    lmbd = 0.85
+    markov_matrix = get_markov_matrix(graph_edges)
+    modified_markov_matrix = get_modified_markov_matrix(lmbd, markov_matrix)
+    dimension = len(markov_matrix)
+    initial_solution = numpy.full(dimension, 1 / dimension)
+    while True:
+        next_solution = numpy.matmul(modified_markov_matrix, initial_solution)
+        if numpy.allclose(next_solution, initial_solution, epsilon, 0):
+            return next_solution
+        initial_solution = next_solution
+
+
+def get_modified_markov_matrix(lmbd, markov_matrix):
+    dimension = len(markov_matrix)
+    return lmbd * markov_matrix + (1 - lmbd) * numpy.full((dimension, dimension), 1 / dimension)
+
+
 def get_markov_matrix(graph_edges):
     vertices_number = count_vertices(graph_edges)
     references_counts = count_references(graph_edges)
@@ -12,7 +30,6 @@ def get_markov_matrix(graph_edges):
             from_vertex = j + 1
             if exists_edge_between(graph_edges, from_vertex, to_vertex):
                 markov_matrix[i][j] = 1 / references_counts.get(from_vertex)
-            print(markov_matrix)
     return markov_matrix
 
 
@@ -21,7 +38,6 @@ def count_references(graph_edges):
 
 
 def exists_edge_between(edges, from_vertex, to_vertex):
-    print(len(edges))
     for index in range(len(edges)):
         if numpy.array_equal(numpy.array([from_vertex, to_vertex]), edges[index, :]):
             return True
